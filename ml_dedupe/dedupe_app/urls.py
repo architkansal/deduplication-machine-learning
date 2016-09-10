ff1cbded5f514609e7165b90e91e35d1dd34becc
architkansal <archit.knsl@gmail.com>
2016-09-10 17:07:15 +0530

    upload page ]

--- ml_dedupe/dedupe_app/urls.py
+++ ml_dedupe/dedupe_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^home/',views.home, name='home'),
+    url(r'^index/',views.index, name='index'),
 ]
--- ml_dedupe/dedupe_app/views.py
+++ ml_dedupe/dedupe_app/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
-
+from django.conf import settings
+from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, Http404,JsonResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.models import User
+from django.db.models import Count, Min, Sum, Avg
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+	return render(request,'home.html')
