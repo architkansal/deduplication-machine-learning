64cd15417bfd6d6fa6aa99f199dd12beea28e47c
architkansal <archit.knsl@gmail.com>
2016-09-10 18:29:22 +0530

    file upload working

--- ml_dedupe/dedupe_app/views.py
+++ ml_dedupe/dedupe_app/views.py
@@ -5,10 +5,46 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.contrib.auth.models import User
 from django.db.models import Count, Min, Sum, Avg
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import os.path
+import sys
+import re
+from django.core.files.base import ContentFile
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def home(request):
-	return render(request,'home.html')
+	return render(request, 'home.html', RequestContext(request, locals())) 
+
+
+def m(request):
+	print request.FILES['csv_file']
+	folder = 'tmp_dir2/' #request.path.replace("/", "_")
+	uploaded_filename = request.FILES['csv_file'].name
+	BASE_PATH = '/home//archit/django-user/deduplication-machine-learning/ml_dedupe'
+	# create the folder if it doesn't exist.
+	try:
+		os.mkdir(os.path.join(BASE_PATH, folder))
+	except:
+		pass
+
+	# save the uploaded file inside that folder.
+	full_filename = os.path.join(BASE_PATH, folder, uploaded_filename)
+	fout = open(full_filename, 'wb+')
+	file_content = ContentFile( request.FILES['csv_file'].read() )
+
+	try:
+	# Iterate through the chunks.
+		for chunk in file_content.chunks():
+			fout.write(chunk)
+		fout.close()
+		html = "<html><body>SAVED</body></html>"
+		return HttpResponse(html)
+	except:
+		html = "<html><body>NOT SAVED</body></html>"
+		return HttpResponse(html)
