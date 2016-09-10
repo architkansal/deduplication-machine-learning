d3932cc330b51669348037b046dd9195666a7468
architkansal <archit.knsl@gmail.com>
2016-09-10 23:13:38 +0530

    jcff

--- ml_dedupe/dedupe_app/views.py
+++ ml_dedupe/dedupe_app/views.py
@@ -91,15 +91,15 @@ def m(request):
 def do_dedupe():
 	optp = optparse.OptionParser()
 	optp.add_option('-v', '--verbose', dest='verbose', action='count',
-                	help='Increase verbosity (specify multiple times for more)'
-                	)
+					help='Increase verbosity (specify multiple times for more)'
+					)
 	(opts, args) = optp.parse_args()
 	log_level = logging.WARNING 
 	if opts.verbose:
-	    if opts.verbose == 1:
-	    	log_level = logging.INFO
-	    elif opts.verbose >= 2:
-	        log_level = logging.DEBUG
+		if opts.verbose == 1:
+			log_level = logging.INFO
+		elif opts.verbose >= 2:
+			log_level = logging.DEBUG
 	logging.getLogger().setLevel(log_level)
 	# ## Setup
 
@@ -116,101 +116,120 @@ def do_dedupe():
 		print('reading from', settings_file)
 		with open(settings_file, 'rb') as f:
 			deduper = dedupe.StaticDedupe(f)
-	else:
-		print 'start training...'
+	else:    # ## Training
+		print 'doing...'
+	# Define the fields dedupe will pay attention to
 		for k,v in data.iteritems():
-			fields = [ {'field' : k, 'type': v }, ]
-
+			fields = [
+				{'field' : k, 'type': v },
+				]
+		print fields
+	# Create a new deduper object and pass our data model to it.
 		deduper = dedupe.Dedupe(fields)
 
-		 # To train dedupe, we feed it a sample of records.
+	# To train dedupe, we feed it a sample of records.
 		deduper.sample(data_d, 15000)
 
-	    # If we have training data saved from a previous run of dedupe,
-	    # look for it and load it in.
-	    # __Note:__ if you want to train from scratch, delete the training_file
-	    if os.path.exists(training_file):
-	        print('reading labeled examples from ', training_file)
-	        with open(training_file, 'rb') as f:
-	            deduper.readTraining(f)
+	# If we have training data saved from a previous run of dedupe,
+	# look for it and load it in.
+	# __Note:__ if you want to train from scratch, delete the training_file
+		if os.path.exists(training_file):
+			print('reading labeled examples from ', training_file)
+			with open(training_file, 'rb') as f:
+				deduper.readTraining(f)
 
-	    # ## Active learning
-	    # Dedupe will find the next pair of records
-	    # it is least certain about and ask you to label them as duplicates
-	    # or not.
-	    # use 'y', 'n' and 'u' keys to flag duplicates
-	    # press 'f' when you are finished
-	    print('starting active labeling...')
+	# ## Active learning
+	# Dedupe will find the next pair of records
+	# it is least certain about and ask you to label them as duplicates
+	# or not.
+	# use 'y', 'n' and 'u' keys to flag duplicates
+	# press 'f' when you are finished
+		print('starting active labeling...')
 
-	    dedupe.consoleLabel(deduper)
+		dedupe.consoleLabel(deduper)
 
-	    # Using the examples we just labeled, train the deduper and learn
-	    # blocking predicates
-	    deduper.train()
+	# Using the examples we just labeled, train the deduper and learn
+	# blocking predicates
+		deduper.train()
 
-	    # When finished, save our training to disk
-	    with open(training_file, 'w') as tf:
-	        deduper.writeTraining(tf)
+	# When finished, save our training to disk
+		with open(training_file, 'w') as tf:
+			deduper.writeTraining(tf)
 
-    # Save our weights and predicates to disk.  If the settings file
-    # exists, we will skip all the training and learning next time we run
-    # this file.
-    with open(settings_file, 'wb') as sf:
-        deduper.writeSettings(sf)
-	
+	# Save our weights and predicates to disk.  If the settings file
+	# exists, we will skip all the training and learning next time we run
+	# this file.
+		with open(settings_file, 'wb') as sf:
+			deduper.writeSettings(sf)
+
+	# Find the threshold that will maximize a weighted average of our
+	# precision and recall.  When we set the recall weight to 2, we are
+	# saying we care twice as much about recall as we do precision.
+	#
+	# If we had more data, we would not pass in all the blocked data into
+	# this function but a representative sample.
+
 	threshold = deduper.threshold(data_d, recall_weight=1)
 
-	
+	# ## Clustering
+
+	# `match` will return sets of record IDs that dedupe
+	# believes are all referring to the same entity.
+
 	print('clustering...')
 	clustered_dupes = deduper.match(data_d, threshold)
 
 	print('# duplicate sets', len(clustered_dupes))
 
-	
+	# ## Writing Results
+
+	# Write our original data back out to a CSV with a new column called 
+	# 'Cluster ID' which indicates which records refer to each other.
+
 	cluster_membership = {}
 	cluster_id = 0
 	for (cluster_id, cluster) in enumerate(clustered_dupes):
-	    id_set, scores = cluster
-	    cluster_d = [data_d[c] for c in id_set]
-	    canonical_rep = dedupe.canonicalize(cluster_d)
-	    for record_id, score in zip(id_set, scores):
-	        cluster_membership[record_id] = {
-	            "cluster id" : cluster_id,
-	            "canonical representation" : canonical_rep,
-	            "confidence": score
-	        }
+		id_set, scores = cluster
+		cluster_d = [data_d[c] for c in id_set]
+		canonical_rep = dedupe.canonicalize(cluster_d)
+		for record_id, score in zip(id_set, scores):
+			cluster_membership[record_id] = {
+				"cluster id" : cluster_id,
+				"canonical representation" : canonical_rep,
+				"confidence": score
+			}
 
 	singleton_id = cluster_id + 1
 
 	with open(output_file, 'w') as f_output, open(input_file) as f_input:
-	    writer = csv.writer(f_output)
-	    reader = csv.reader(f_input)
+		writer = csv.writer(f_output)
+		reader = csv.reader(f_input)
 
-	    heading_row = next(reader)
-	    heading_row.insert(0, 'confidence_score')
-	    heading_row.insert(0, 'Cluster ID')
-	    canonical_keys = canonical_rep.keys()
-	    for key in canonical_keys:
-	        heading_row.append('canonical_' + key)
+		heading_row = next(reader)
+		heading_row.insert(0, 'confidence_score')
+		heading_row.insert(0, 'Cluster ID')
+		canonical_keys = canonical_rep.keys()
+		for key in canonical_keys:
+			heading_row.append('canonical_' + key)
 
-	    writer.writerow(heading_row)
+		writer.writerow(heading_row)
 
-	    for row in reader:
-	        row_id = int(row[0])
-	        if row_id in cluster_membership:
-	            cluster_id = cluster_membership[row_id]["cluster id"]
-	            canonical_rep = cluster_membership[row_id]["canonical representation"]
-	            row.insert(0, cluster_membership[row_id]['confidence'])
-	            row.insert(0, cluster_id)
-	            for key in canonical_keys:
-	                row.append(canonical_rep[key].encode('utf8'))
-	        else:
-	            row.insert(0, None)
-	            row.insert(0, singleton_id)
-	            singleton_id += 1
-	            for key in canonical_keys:
-	                row.append(None)
-	        writer.writerow(row)
+		for row in reader:
+			row_id = int(row[0])
+			if row_id in cluster_membership:
+				cluster_id = cluster_membership[row_id]["cluster id"]
+				canonical_rep = cluster_membership[row_id]["canonical representation"]
+				row.insert(0, cluster_membership[row_id]['confidence'])
+				row.insert(0, cluster_id)
+				for key in canonical_keys:
+					row.append(canonical_rep[key].encode('utf8'))
+			else:
+				row.insert(0, None)
+				row.insert(0, singleton_id)
+				singleton_id += 1
+				for key in canonical_keys:
+					row.append(None)
+			writer.writerow(row)
 
 
 def preProcess(column):
@@ -256,7 +275,6 @@ def readData(filename):
 def got_it(request):
 	attr="attr"
 	comp="comp"
-	data={}
 	for x in range(1,10):
 		comp+=str(x)
 		attr+=str(x)
